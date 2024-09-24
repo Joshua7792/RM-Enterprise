164455bccf5d3912d3d88e364c35b6fc69c7d1aa
joshua7792 <j.santiago39@hotmail.com>
2024-09-24 00:12:18 -0400

    commit for changes

--- rm_showcase/baseball/views.py
+++ rm_showcase/baseball/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
-from .models import Video, Article
 from django.http import JsonResponse
+from .models import Video
 
 def home(request):
-    videos = Video.objects.all()
-    articles = Article.objects.all()
-    return render(request, 'home.html', {'videos': videos, 'articles': articles})
+    return render(request, 'home.html')
 
 def load_videos(request):
     videos = list(Video.objects.values())
--- rm_showcase/settings.py
+++ rm_showcase/settings.py
@@ -124,7 +124,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
